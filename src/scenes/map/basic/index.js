63a1e83334982d195342fadbffeca490f0853a2b
lighteagle <sofyan.agus18@gmail.com>
2023-06-19 00:24:50 +0700

    add several basemap on basic map

--- src/scenes/map/basic/index.js
+++ src/scenes/map/basic/index.js
@@ -4,6 +4,73 @@ import 'leaflet/dist/leaflet.css';
 import LeafletControlGeocoder from '../../../component/LeafletControlGeocoder';
 
 const { BaseLayer } = LayersControl;
+const layerList = [
+  {
+    name: "Midnight",
+    url: "https://api.mapbox.com/styles/v1/koltigis/clj1bnr9800sr01qy6fn831ky/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: true,
+  },
+  {
+    name: "Hillshade",
+    url: "https://api.mapbox.com/styles/v1/koltigis/clciuvvw9001l14mw3p0uhl2e/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Streets",
+    url: "https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Outdoors",
+    url: "https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Light",
+    url: "https://api.mapbox.com/styles/v1/mapbox/light-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Dark",
+    url: "https://api.mapbox.com/styles/v1/mapbox/dark-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Satellite",
+    url: "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Satellite Streets",
+    url: "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v12/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Navigation Day",
+    url: "https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Navigation Night",
+    url: "https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Pencil",
+    url: "https://api.mapbox.com/styles/v1/koltigis/clj1ov8tf01eo01r17qjv6b6r/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Moonlight",
+    url: "https://api.mapbox.com/styles/v1/mapbox/cj3kbeqzo00022smj7akz3o1e/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+  {
+    name: "Blue Print",
+    url: "https://api.mapbox.com/styles/v1/mapbox-map-design/cks97e1e37nsd17nzg7p0308g/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA",
+    checked: false,
+  },
+];
 
 function BasicMap() {
   return (
@@ -12,14 +79,14 @@ function BasicMap() {
         center={[0, 0]}
         zoom={2}
         style={{ height: '100vh', width: '100%' }}
-        zoomControl={false} // Disable the zoom control
+        zoomControl={false}
       >
         <LayersControl position="topleft">
-          <BaseLayer name="Midnight" checked>
-            <TileLayer
-              url="https://api.mapbox.com/styles/v1/koltigis/clj1bnr9800sr01qy6fn831ky/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia29sdGlnaXMiLCJhIjoiY2xhYzYyNm9xMDc5czNvbWhjMTNrODVkMyJ9.2Ni9VMsNgMf3vj67B_l2zA"
-            />
-          </BaseLayer>
+          {layerList.map((layer, index) => (
+            <BaseLayer key={index} checked={layer.checked} name={layer.name}>
+              <TileLayer url={layer.url} />
+            </BaseLayer>
+          ))}
         </LayersControl>
         <LeafletControlGeocoder />
       </MapContainer>
