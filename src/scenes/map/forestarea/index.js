45251e414e980b4bbbe77f1fe3c9108e1f0e4a1f
sofyansalim <sofyan.salim@koltiva.com>
2023-12-13 10:01:05 +0700

    update forest area

--- src/scenes/map/forestarea/index.js
+++ src/scenes/map/forestarea/index.js
@@ -4,22 +4,37 @@ import 'leaflet/dist/leaflet.css';
 import LeafletControlGeocoder from '../../../component/LeafletControlGeocoder';
 import L from 'leaflet';
 
-const baseUrl = 'https://tiles.globalforestwatch.org';
-const layerTemplate = {
-  tcl             : `${baseUrl}/umd_tree_cover_loss/latest/tcd_30/{z}/{x}/{y}.png`,
-  forest          : `${baseUrl}/umd_regional_primary_forest_2001/latest/dynamic/{z}/{x}/{y}.png`,
-  carbonEmmision  : `${baseUrl}/gfw_forest_carbon_gross_removals/latest/tcd_30/{z}/{x}/{y}.png`
-};
-
 // https://forest-observatory.ec.europa.eu/wms/GFC2020?service=WMS&request=GetMap&layers=forest&styles=&format=image%2Fpng&transparent=true&version=1.3.0&srs=EPSG%3A4326&layerId=forestundefined&width=256&height=256&crs=EPSG%3A4326&bbox=0,67.5,22.5,90
 
 // https://forest-observatory.ec.europa.eu/wms/GFA?service=WMS&request=GetMap&layers=GFM&styles=&format=image%2Fpng&transparent=true&version=1.3.0&width=256&height=256&bbox=-3.427734375%2C119.00390625%2C-3.33984375%2C119.091796875&crs=EPSG%3A4326&bbox=-85.0511287798066,0,-66.51326044311186,90
 
+// https://forest-observatory.ec.europa.eu/wms/GFA?service=WMS&request=GetMap&layers=GFF&styles=&format=image%2Fpng&transparent=true&version=1.3.0&srs=EPSG%3A4326&layerId=fragmentationundefined&width=256&height=256&crs=EPSG%3A4326&bbox=0,135,22.5,157.5
 
+const wmsUrl2020 = 'https://forest-observatory.ec.europa.eu/wms/GFC2020';
+const wmsOptions2020 = {
+    layers: 'forest',
+    format: 'image/png',
+    transparent: true,
+    version: '1.3.0',
+    crs: L.CRS.EPSG4326,
+    width: 256,
+    height: 256,
+    bbox: [-3.427734375, 119.00390625, -3.33984375, 119.091796875],
+  };
 const wmsUrl = 'https://forest-observatory.ec.europa.eu/wms/GFA';
-const wmsLayers = 'GFM';
 const wmsOptions = {
-    layers: wmsLayers,
+    layers: 'GFM',
+    format: 'image/png',
+    transparent: true,
+    version: '1.3.0',
+    crs: L.CRS.EPSG4326,
+    width: 256,
+    height: 256,
+    bbox: [-3.427734375, 119.00390625, -3.33984375, 119.091796875],
+  };
+const wmsUrlFF = 'https://forest-observatory.ec.europa.eu/wms/GFA';
+const wmsOptionsFF = {
+    layers: 'GFF',
     format: 'image/png',
     transparent: true,
     version: '1.3.0',
@@ -81,28 +96,13 @@ function ForestArea() {
 
           
           <Overlay name="Global map of Forest Cover for year 2020" zIndex={10}>
-            <WMSTileLayer url={wmsUrl} {...wmsOptions} />
+            <WMSTileLayer url={wmsUrl2020} {...wmsOptions2020} />
           </Overlay>
           <Overlay name="Forest Mask 2019" zIndex={11}>
             <WMSTileLayer url={wmsUrl} {...wmsOptions} />
           </Overlay>
           <Overlay name="Forest Fragmentation 2019" zIndex={12}>
-            <WMSTileLayer url={wmsUrl} {...wmsOptions} />
-          </Overlay>
-          <Overlay name="Forest Accounting 2019" zIndex={12}>
-            <WMSTileLayer url={wmsUrl} {...wmsOptions} />
-          </Overlay>
-          <Overlay name="Global Map of Forest Cover Changes and their Drivers [2016-2022]" zIndex={12}>
-            <WMSTileLayer url={wmsUrl} {...wmsOptions} />
-          </Overlay>
-          <Overlay name="Annual Change [2016-2022]" zIndex={12}>
-            <WMSTileLayer url={wmsUrl} {...wmsOptions} />
-          </Overlay>
-          <Overlay name="Deforestation Year" zIndex={12}>
-            <WMSTileLayer url={wmsUrl} {...wmsOptions} />
-          </Overlay>
-          <Overlay name="Degradation Year" zIndex={12}>
-            <WMSTileLayer url={wmsUrl} {...wmsOptions} />
+            <WMSTileLayer url={wmsUrlFF} {...wmsOptionsFF} />
           </Overlay>
         </LayersControl>
       </MapContainer>
@@ -111,17 +111,3 @@ function ForestArea() {
 }
 
 export default ForestArea;
-
-/*
-   // Add WMS layer for GFM
-    var gfmLayer = L.tileLayer.wms('https://forest-observatory.ec.europa.eu/wms/GFA', {
-      layers: 'GFM',
-      format: 'image/png',
-      transparent: true,
-      version: '1.3.0',
-      crs: L.CRS.EPSG4326,
-      width: 256,
-      height: 256,
-      bbox: [-3.427734375, 119.00390625, -3.33984375, 119.091796875],
-    });
-*/ 
